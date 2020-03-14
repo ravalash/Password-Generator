07ae50cfb2d8546046259dabae5f5d0251af993d
Ravalash <13k0873juhky@opayq.net>
2020-03-13 21:47:09 -0400

    Adding process to collect number of characters

--- script.js
+++ script.js
@@ -10,5 +10,31 @@ function writePassword() {
 
 }
 
+// Variable Declaration
+// initial character set
+var chars = ""; 
+// Upper case characters
+var uChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Lower case characters
+var lChars = "abcdefghijklmnopqrstuvwxyz";
+// Numerical characters
+var nChars = "0123456789";
+// Symbol Characters
+var sChars = "!@#$%^&*()_+-=";
+
+
+
+
+function generatePassword() {
+  // Capture number of characters. Discard strings, round down integers
+  var nPassword = 0;
+  while ((nPassword < 8 || nPassword > 128) || isNaN(parseInt(nPassword))) {
+  var nPassword = prompt("Please enter a password length between 8 and 128 characters");
+  nPassword=Math.floor(nPassword);
+  }
+
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
